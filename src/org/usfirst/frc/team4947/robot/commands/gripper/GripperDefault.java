d38c821109207d5cc927dd57c6c1e80851e27852
fross <fross.job@gmail.com>
2018-02-17 21:49:12 -0500

    Gripper: maps the controls on the operator controller.
    -Left axis up: closes the gripper.
    -Left axis down: opens the gripper.
    
    -Left trigger: rejects the cube.
    -Right trigger: pulls the cube.

--- src/org/usfirst/frc/team4947/robot/commands/gripper/GripperDefault.java
+++ src/org/usfirst/frc/team4947/robot/commands/gripper/GripperDefault.java
@@ -2,19 +2,13 @@ package org.usfirst.frc.team4947.robot.commands.gripper;
 
 import org.usfirst.frc.team4947.robot.OI.XBoxAxis;
 import org.usfirst.frc.team4947.robot.Robot;
-import org.usfirst.frc.team4947.robot.subsystems.Gripper;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class GripperDefault extends Command {
 	
-	// Members.
-	private Gripper gripper;
-
-	public GripperDefault(Gripper gripper) {
-		requires(gripper);
-		
-		this.gripper = gripper;
+	public GripperDefault() {
+		requires(Robot.gripper);
 	}
 
 	// Called just before the command runs the first time.
@@ -29,31 +23,21 @@ public class GripperDefault extends Command {
 	
 	private void updateOpening() {
 		double leftStickAxisY = Robot.oi.getJoystickHelperAxis(XBoxAxis.LEFT_STICK_Y, 0.1);
-		
-		double value = Math.signum(leftStickAxisY);
-		if (value > 0.5) {
-			gripper.open();
-		} else if (value < 0.5) {
-			gripper.close();
+		if (leftStickAxisY > 0.5) {
+			Robot.gripper.close();
+		} else if (leftStickAxisY < -0.5) {
+			Robot.gripper.open();
 		}
-		
-		System.out.format("helperLeftStickAxisY=%d, value=%d%n", leftStickAxisY, value);
 	}
 	
 	private void updateSpeed() {
-		double leftTrigger = Robot.oi.getJoystickHelperAxis(XBoxAxis.LEFT_TRIGGER);
+		double leftTrigger = -Robot.oi.getJoystickHelperAxis(XBoxAxis.LEFT_TRIGGER);
 		double rightTrigger = Robot.oi.getJoystickHelperAxis(XBoxAxis.RIGHT_TRIGGER);
 		
 		double percentOutput = (leftTrigger + rightTrigger);
 		double direction = Math.signum(Double.compare(rightTrigger, leftTrigger));
 
-		System.out.format("helperleftTrigger=%d, helperRightTrigger=%d, percentOutput=%d, direction=%d%n", 
-				leftTrigger, 
-				rightTrigger,
-				percentOutput,
-				direction);
-
-		gripper.rotate(direction * percentOutput);		
+		Robot.gripper.rotateFlip(direction * percentOutput);
 	}
 	
 	// Called when another command which requires one or more of the same subsystems is scheduled to run.
