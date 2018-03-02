c18c5436d26b4d653f0291ddbe48f57267a28cbe
pc-fenix-2\Fenix <jeanphilippe.choiniere@ge.com>
2018-03-02 16:41:18 -0500

    tuned final approach to avoid penalties

--- src/org/usfirst/frc/team4947/robot/commands/autonomous/DriveFinalApproach.java
+++ src/org/usfirst/frc/team4947/robot/commands/autonomous/DriveFinalApproach.java
@@ -33,7 +33,8 @@ public class DriveFinalApproach extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return (isTimedOut() || Robot.gripper.isCubePresent()) ;
+        //return (isTimedOut() || Robot.gripper.isCubePresent()) ;
+    	return isTimedOut();
     }
 
     // Called once after isFinished returns true
